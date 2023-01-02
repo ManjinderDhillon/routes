4ef4f9ffc7dd320400371556a9e6d5e71faf8a2e
manjinder <manjinder@xyrintech.com>
2023-01-02 08:49:54 +0530

    vuex

---
+++ NewVuex/demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>{{ $store.state.counter }}</h3>
+  <button @click="addOne">Add</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  methods: {
+    addOne() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.counter);
+    },
+  },
+};
+</script>
---
+++ NewVuex/demo/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>{{ $store.state.counter }}</h3>
+  <button @click="addOne">Add</button>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  methods: {
+    addOne() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.counter);
+    },
+  },
+};
+</script>
